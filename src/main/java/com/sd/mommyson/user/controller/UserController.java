d2e928584ef9b57f656ea924edc2d55a56b155bf
YangYunJe <yhee1030@gmail.com>
2021-12-01 22:57:56 +0900

    커밋

---
+++ src/main/java/com/sd/mommyson/user/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.sd.mommyson.user.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.sd.mommyson.user.service.UserService;
+
+@Controller("/user/*")
+public class UserController {
+	
+	private UserService userService;
+	
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@RequestMapping("myOrderList")
+	public String myOrderList() {
+		
+		return "userReview";
+	}
+
+}
