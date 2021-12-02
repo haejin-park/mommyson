d7f6694ad7957fbe0f97a45f19c821c77c17cf7e
DESKTOP-EQTN589\ShinHyungi <sabbath4682@naver.com>
2021-12-02 16:52:32 +0900

    cart

--- src/main/java/com/sd/mommyson/user/controller/UserController.java
+++ src/main/java/com/sd/mommyson/user/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.sd.mommyson.user.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +16,10 @@ public class UserController {
 	}
 	
 	@GetMapping("category/{category}")
-	public void categoryPage(ModelAndView model,@PathVariable String category) {
-		model.addObject("category", category);
-		model.setViewName("user/category_page");
+	public ModelAndView categoryPage(ModelAndView mv,@PathVariable String category) {
+		mv.addObject("category", category);
+		mv.setViewName("user/category_page");
+		
+		return mv;
 	}
 }
