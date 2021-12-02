05e6aed4a7c3911b2dc6b2bbdfbe9fcba641b6e8
LAPTOP-KJ015908\leeseungwoo <vwnua@naver.com>
2021-12-03 01:16:41 +0900

    설정관리 카테고리 설정 추가, 메인페이지 css 수정 2021/12/03 새벽 1시 16분 승우 커밋

--- src/main/java/com/sd/mommyson/manager/controller/ManagerController.java
+++ src/main/java/com/sd/mommyson/manager/controller/ManagerController.java
@@ -8,7 +8,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/manager/*")
 public class ManagerController {
 	
-	/* 관리자  페이지 */
+	/* 일반회원조회 */
 	@GetMapping("normalMember")
 	public void normalMember() {}
+	
+	/* 배너설정 */
+	@GetMapping("bannerManage")
+	public void bannerManage() {}
+	
+	/* 태그설정 */
+	@GetMapping("tagManage")
+	public void tagManage() {}
+	
+	/* 카테고리 설정 */
+	@GetMapping("categoryManage")
+	public void categoryManage() {}
 }
