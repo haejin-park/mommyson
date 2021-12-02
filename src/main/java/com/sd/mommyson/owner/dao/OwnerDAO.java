136ac18d7a2da9db24b766a5332d3bfb280d0426
YangYunJe <yhee1030@gmail.com>
2021-12-02 18:37:34 +0900

    임시커밋 지금 이거 받지 말것

--- src/main/java/com/sd/mommyson/owner/dao/OwnerDAO.java
+++ src/main/java/com/sd/mommyson/owner/dao/OwnerDAO.java
@@ -1,9 +1,9 @@
-package com.sd.mommyson.owner.dao;
-
-import com.sd.mommyson.member.dto.MemberDTO;
-
-public interface OwnerDAO {
-
-	public MemberDTO selectOwner(MemberDTO member);
-	
-}
+//package com.sd.mommyson.owner.dao;
+//
+//import com.sd.mommyson.member.dto.MemberDTO;
+//
+//public interface OwnerDAO {
+//
+//	public MemberDTO selectOwner(MemberDTO member);
+//	
+//}
