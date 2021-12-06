abafa21be2bd37f29ace07f2c97bf516c7b41492
DESKTOP-EQTN589\ShinHyungi <sabbath4682@naver.com>
2021-12-06 21:53:23 +0900

    c

--- src/main/java/com/sd/mommyson/user/dao/UserDAO.java
+++ src/main/java/com/sd/mommyson/user/dao/UserDAO.java
@@ -4,10 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.sd.mommyson.manager.dto.PostDTO;
-<<<<<<< HEAD
 import com.sd.mommyson.user.common.SelectCriteria;
-=======
->>>>>>> 4ed0ea00577004a0d636d07a9240ea52c36002b9
 
 public interface UserDAO {
 
